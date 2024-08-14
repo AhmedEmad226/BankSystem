4430c1a01d3d1901f3a74da12d6094bb1e70f357
mohandelwakeel7 <mohandelwakeel7@gmail.com>
2024-08-14 17:19:41 +0300

    test

--- main.cpp
+++ main.cpp
@@ -7,13 +7,4 @@ class Test{
 };
 
 int main()
-<<<<<<< Updated upstream
 {cout<<"mohand"
-=======
-{
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
-
->>>>>>> 3fdac9ab6589e5f7ace7db7710828cfed183ebfd
-}
